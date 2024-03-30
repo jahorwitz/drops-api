870b63a00126cb2166a543850b2b7108e344befa
Josh Horwitz <josh.horwitz@floqast.com>
2024-03-30 00:16:15 -0400

    feat: initial commit

---
+++ keystone.ts
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import { config } from "@keystone-6/core";
+
+dotenv.config();
+
+import { withAuth, session } from "./auth";
+import * as Models from "./models";
+
+export default withAuth(
+  config({
+    server: {
+      port: 8080,
+      cors: {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      },
+    },
+    db: {
+      provider: "mysql",
+      url: `mysql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`,
+      enableLogging: true,
+      idField: { kind: "uuid" },
+      useMigrations: true,
+    },
+    telemetry: false,
+    graphql: {
+      playground: true,
+      apolloConfig: {
+        introspection: true,
+      },
+    },
+    storage: {
+      s3_file_storage: {
+        kind: "s3",
+        type: "file",
+        bucketName: process.env.S3_BUCKET_NAME || "drops-keystonejs",
+        region: process.env.S3_REGION || "us-east-2",
+        accessKeyId: process.env.S3_ACCESS_KEY_ID || "keystone",
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "keystone",
+        signed: { expiry: 5000 },
+        forcePathStyle: true,
+      },
+      s3_image_storage: {
+        kind: "s3",
+        type: "image",
+        bucketName: process.env.S3_BUCKET_NAME || "drops-keystonejs",
+        region: process.env.S3_REGION || "us-east-2",
+        accessKeyId: process.env.S3_ACCESS_KEY_ID || "keystone",
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "keystone",
+        signed: { expiry: 5000 },
+        forcePathStyle: true,
+      },
+    },
+    lists: Models,
+    session,
+  }),
+);
