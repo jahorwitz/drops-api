a32f5034b5790d241367891116f26a754a54f7da
iankamar <ian.kamar@outlook.com>
2024-04-19 13:41:10 -0400

    fix(component): update component

--- models/user.ts
+++ models/user.ts
@@ -1,14 +1,15 @@
 import { list } from "@keystone-6/core";
 import type { ListConfig } from "@keystone-6/core";
-import type { Lists } from ".keystone/types";
+
 import {
   checkbox,
   text,
   password,
   timestamp,
+  relationship,
 } from "@keystone-6/core/fields";
 
-export const User: ListConfig<Lists.User.TypeInfo<any>, any> = list({
+export const User: ListConfig<any, any> = list({
   access: {
     operation: {
       query: ({ session }) => !!session?.data.isAdmin,
@@ -38,5 +39,6 @@ export const User: ListConfig<Lists.User.TypeInfo<any>, any> = list({
     lastLoginDate: timestamp({
       defaultValue: { kind: "now" },
     }),
+    activities: relationship({ref: 'Activity.user', many:true}),
   },
 });
