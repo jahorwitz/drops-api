3922fb06eb215e54277741b6a5e9fe2611260239
Sergio Mendoza <Mendozsa.sm@gmail.com>
2024-07-29 12:42:22 -0400

    fix: updated activity & user models

--- models/activity.ts
+++ models/activity.ts
@@ -7,7 +7,7 @@ import {
   relationship,
 } from "@keystone-6/core/fields";
 
-export const Activity: ListConfig<Lists.Activity.TypeInfo<any>, any> = list({
+export const Activity: ListConfig<any> = list({
   access: {
     operation: {
       query: ({ session }) => !!session,
