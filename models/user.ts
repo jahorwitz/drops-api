6e8ef911fbc7b8f25f27831fd6b9a7754500c7fe
MrSurge87 <Mendozsa.sm@gmail.com>
2024-08-04 20:08:29 -0400

    fix: fix linting errors

--- models/user.ts
+++ models/user.ts
@@ -1,7 +1,13 @@
 import { list } from "@keystone-6/core";
 import type { ListConfig } from "@keystone-6/core";
 import type { Lists } from ".keystone/types";
-import { checkbox, text, password, timestamp, relationship } from "@keystone-6/core/fields";
+import {
+  checkbox,
+  text,
+  password,
+  timestamp,
+  relationship,
+} from "@keystone-6/core/fields";
 
 export const User: ListConfig<Lists.User.TypeInfo<any>, any> = list({
   access: {
