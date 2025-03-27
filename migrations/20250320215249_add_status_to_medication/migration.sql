343fd5269de11f193d31452289a3e32764594f32
Telasja Reynolds <telasjareynolds@gmail.com>
2025-03-26 20:44:03 -0400

    style: run prettier to pass pre-commit hook

---
+++ migrations/20250320215249_add_status_to_medication/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `medication` ADD COLUMN `status` BOOLEAN NOT NULL DEFAULT false;
--- models/medication.ts
+++ models/medication.ts
@@ -1,7 +1,7 @@
 import { list } from "@keystone-6/core";
 import type { ListConfig } from "@keystone-6/core";
 import type { Lists } from ".keystone/types";
-import { integer, relationship, text } from "@keystone-6/core/fields";
+import { integer, relationship, text, checkbox } from "@keystone-6/core/fields";
 
 export const Medication: ListConfig<Lists.Medication.TypeInfo<any>, any> = list(
   {
@@ -34,6 +34,7 @@ export const Medication: ListConfig<Lists.Medication.TypeInfo<any>, any> = list(
       unitOfMeasure: text({ validation: { isRequired: true } }),
       time: text({ validation: { isRequired: true } }),
       user: relationship({ ref: "User.medications", many: false }),
+      status: checkbox({ defaultValue: false }),
     },
   },
 );
